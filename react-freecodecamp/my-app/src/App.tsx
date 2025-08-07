a73d36e7fca4e9543a7187e08bf3b84062f3c764
scmgustafson <scmgustafson@gmail.com>
2025-08-07 06:54:39 -0700

    Finish props 2

--- react-freecodecamp/my-app/src/App.tsx
+++ react-freecodecamp/my-app/src/App.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const Title = (): any => {
+  return <div>Title!</div>;
+};
+
+const Description = (): any => {
+  console.log("Description console text");
+  return <div>Here is some description for the app</div>;
+};
+
+const Message = (props: any) => {
+  const { text, bgColor } = props; // this is destructuring the dict props {text: "balh", color: "blah"}
+
+  console.log(props);
+  // curly braces below are used for rendering any javascript
+  return <div style={{ background: bgColor }}>{text}</div>;
+};
+
+const Box = (props: any) => {
+  const { label, bgColor, textColor, large } = props;
+  return (
+    <div
+      style={{
+        background: bgColor,
+        color: textColor,
+        fontSize: large ? "40px" : "20px",
+      }}
+    >
+      {label}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Box
+          label="My name is boxboy"
+          bgColor="CornFlowerBlue"
+          textColor="White"
+          large={true}
+        />
+        <Box
+          label="and I am box girl"
+          bgColor="Pink"
+          textColor="Black"
+          large={false}
+        />
       </header>
     </div>
   );
