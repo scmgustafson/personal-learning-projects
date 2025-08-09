8dacbb79d5963b74106f8e33812c38f154afab2d
scmgustafson <scmgustafson@gmail.com>
2025-08-09 15:35:44 -0700

    Finish tailwind button lesson

--- react-freecodecamp/my-app/src/App.tsx
+++ react-freecodecamp/my-app/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import MyComponent from "./MyComponent";
+import Button from "./Button";
 
 const Title = (): any => {
   return <div>Title!</div>;
@@ -34,21 +35,13 @@ const Box = (props: any) => {
 };
 
 function App() {
+  const error = false;
+
   return (
     <div className="App">
       <header className="App-header">
-        <button className="text-green-700 border-2 border-green-700 px-8 py-4 m-4 rounded-md">
-          Forest
-        </button>
-        <button className="text-white bg-sky-500 border-sky-300 border-2 px-8 py-4 m-4 rounded-md">
-          Sky
-        </button>
-        <button className="text-red-500 bg-red-800 p-8 rounded-full hover:bg-red-500 hover:text-red-100 m-4">
-          Fire
-        </button>
-        <button className="text-violet-900 bg-violet-400 rounded-full py-2 px-10 m-4">
-          Magic
-        </button>
+        <Button large>Big!</Button>
+        <Button>Small.</Button>
       </header>
     </div>
   );
--- react-freecodecamp/my-app/src/Button.tsx
+++ react-freecodecamp/my-app/src/Button.tsx
@@ -1,6 +1,13 @@
 const Button = (props: any) => {
-  const { bgColor, textColor, text } = props;
-  return <button></button>;
+  const { large, children } = props;
+  const largeStyle = large ? "text-3xl" : "text-xl";
+  return (
+    <button
+      className={`text-white bg-gray-600 border-2 px-6 py-2 m-4 rounded-lg ${largeStyle}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
